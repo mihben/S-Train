cf247b2f76aa381487f29f090af1e30100db0cd0
Mihucza Bence <mihben@gmail.com>
2022-08-11 07:50:17 +0200

    Logging to test output

--- src/STrain.CQS.Test.Function/Support/SampleHost.cs
+++ src/STrain.CQS.Test.Function/Support/SampleHost.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc.Testing;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using STrain.Sample.Backend;
+using Xunit.Abstractions;
 
 namespace STrain.CQS.Test.Unit.Support
 {
     public class SampleHost : WebApplicationFactory<Program>
     {
+        private readonly ITestOutputHelper _outputHelper;
 
+        public SampleHost(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            builder.ConfigureTestServices(services => services.AddLogging(builder => builder
+                                                                                                                    .AddXUnit(_outputHelper)
+                                                                                                                    .SetMinimumLevel(LogLevel.Trace)));
+        }
     }
 }
