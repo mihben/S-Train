f3acebdc9703640f9e44a715a21270b67a2fb9ab
Mihucza Bence <mihben@gmail.com>
2022-09-02 07:23:11 +0200

    Implemented MvcRequestReceiver

---
+++ src/STrain.CQS.MVC/Receiving/MvcRequestReceiver.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using STrain.CQS.Dispatchers;
+using STrain.CQS.Receivers;
+
+namespace STrain.CQS.MVC.Receiving
+{
+    public interface IMvcRequestReceiver : IRequestReceiver<IActionResult> { }
+
+    public class MvcRequestReceiver : IMvcRequestReceiver
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+
+        public MvcRequestReceiver(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDispatcher)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDispatcher;
+        }
+
+        public async Task<IActionResult> ReceiveAsync<TCommand>(TCommand command, CancellationToken cancellationToken) where TCommand : Command
+        {
+            await _commandDispatcher.DispatchAsync(command, cancellationToken);
+            return new AcceptedResult();
+        }
+
+        public async Task<IActionResult> ReceiveAsync<TQuery, T>(TQuery query, CancellationToken cancellationToken) where TQuery : Query<T>
+        {
+            return new OkObjectResult(await _queryDispatcher.DispatchAsync(query, cancellationToken));
+        }
+    }
+}
---
+++ src/STrain.CQS/Receivers/IRequestReceiver.cs
@@ -0,0 +1,10 @@
+﻿namespace STrain.CQS.Receivers
+{
+    public interface IRequestReceiver<TResponse>
+    {
+        Task<TResponse> ReceiveAsync<TCommand>(TCommand command, CancellationToken cancellationToken)
+            where TCommand : Command;
+        Task<TResponse> ReceiveAsync<TQuery, T>(TQuery query, CancellationToken cancellationToken)
+            where TQuery : Query<T>;
+    }
+}
