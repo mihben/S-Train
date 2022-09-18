016fd25661c5d9e6e06d4f5ec9dc934330ac84ed
Mihucza Bence <mihben@gmail.com>
2022-09-18 17:48:10 +0200

    Implemented AttributiveQueryParameterBinder

---
+++ src/STrain.CQS.Http/RequestSending/Binders/Attributive/AttributiveQueryParameterBinder.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Reflection;
+using System.Web;
+
+namespace STrain.CQS.Http.RequestSending.Binders.Attributive
+{
+    public class AttributiveQueryParameterBinder : IQueryParameterBinder
+    {
+        private readonly ILogger<AttributiveQueryParameterBinder> _logger;
+
+        public AttributiveQueryParameterBinder(ILogger<AttributiveQueryParameterBinder> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<string?> BindAsync<TRequest>(TRequest request, CancellationToken cancellationToken)
+            where TRequest : IRequest
+        {
+            _logger.LogDebug("Binding query parameters");
+
+            var type = typeof(TRequest);
+            string? result;
+            if (type.GetCustomAttribute<QueryParameterAttribute>() is not null) result = request.SerializeToQueryString();
+            else result = request.SerializePropertiesToQueryString();
+
+            _logger.LogTrace("Query parameter: {query}", result);
+            return Task.FromResult(result);
+        }
+    }
+
+    internal static class AttributiveQueryParameterBinderExtensions
+    {
+        public static string? SerializeToQueryString<TRequest>(this TRequest request)
+        {
+            var type = typeof(TRequest);
+            var result = HttpUtility.ParseQueryString(string.Empty);
+            foreach (var property in type.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+            {
+                var attribute = property.GetCustomAttribute<QueryParameterAttribute>();
+                var value = property.GetValue(request);
+                if (value is not null) result.Add(attribute?.Name ?? property.Name, value.ToString());
+            }
+            return result.ToString();
+        }
+
+        public static string? SerializePropertiesToQueryString<TRequest>(this TRequest request)
+            where TRequest : IRequest
+        {
+            var result = HttpUtility.ParseQueryString(string.Empty);
+            foreach (var property in typeof(TRequest).GetProperties(BindingFlags.Instance | BindingFlags.Public)
+                                        .Where(p => p.GetCustomAttribute<QueryParameterAttribute>() is not null))
+            {
+                var attribute = property.GetCustomAttribute<QueryParameterAttribute>();
+                var value = property.GetValue(request);
+                if (value is not null) result.Add(attribute?.Name ?? property.Name, value.ToString());
+            }
+            return result.ToString();
+        }
+    }
+}
---
+++ src/STrain.CQS.Test.Unit/Http/RequestSending/AttributiveQueryParameterBinderTest.cs
@@ -0,0 +1,96 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using STrain.CQS.Http.RequestSending.Binders.Attributive;
+using STrain.CQS.Test.Unit.CQS;
+using Xunit.Abstractions;
+
+namespace STrain.CQS.Test.Unit.Http.RequestSending
+{
+    public class AttributiveQueryParameterBinderTest
+    {
+        private readonly ILogger<AttributiveQueryParameterBinder> _logger;
+
+        public AttributiveQueryParameterBinderTest(ITestOutputHelper outputHelper)
+        {
+            _logger = new LoggerFactory()
+                            .AddXUnit(outputHelper)
+                            .CreateLogger<AttributiveQueryParameterBinder>();
+        }
+
+        private AttributiveQueryParameterBinder CreateSUT()
+        {
+            return new AttributiveQueryParameterBinder(_logger);
+        }
+
+        [Fact(DisplayName = "[UNIT][AQPB-001] - Bind whole object")]
+        public async Task AttributiveQueryParameterBinder_BindAsync_BindQueryParameter()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var request = new Fixture().Create<QueryParameterRequest>();
+
+            // Act
+            var result = await sut.BindAsync(request, default);
+
+            // Assert
+            Assert.Equal($"{nameof(QueryParameterRequest.ByName)}={request.ByName}&by-attribute={request.ByAttribute}", result);
+        }
+
+        [Fact(DisplayName = "[UNIT][AQPB-002] - Bind property")]
+        public async Task AttributiveQueryParameterBinder_BindAsync_BindProperty()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var request = new Fixture().Create<PropertyQueryParameterRequest>();
+
+            // Act
+            var result = await sut.BindAsync(request, default);
+
+            // Assert
+            Assert.Equal($"{nameof(PropertyQueryParameterRequest.ByName)}={request.ByName}&by-attribute={request.ByAttribute}", result);
+        }
+
+        [Fact(DisplayName = "[UNIT][AQPB-003] - Skip null value")]
+        public async Task AttributiveQueryParameterBinder_BindAsync_SkipNullValue()
+        {
+            // Arrange
+            var sut = CreateSUT();
+
+            // Act
+            var result = await sut.BindAsync(new Fixture().Build<QueryParameterRequest>().Without(r => r.ByName).Without(r => r.ByAttribute).Create(), default);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [Fact(DisplayName = "[UNIT][AQPB-004] - Without query parameter")]
+        public async Task AttributiveQueryParameterBinder_BindAsync_WithoutQueryParameter()
+        {
+            // Arrange
+            var sut = CreateSUT();
+
+            // Act
+            var result = await sut.BindAsync(new Fixture().Create<TestRequest>(), default);
+
+            // Assert
+            Assert.Empty(result);
+        }
+
+        [QueryParameter]
+        private record QueryParameterRequest : IRequest
+        {
+            public string ByName { get; set; }
+            [QueryParameter(Name = "by-attribute")]
+            public string ByAttribute { get; set; }
+        }
+
+        private record PropertyQueryParameterRequest : IRequest
+        {
+            [QueryParameter]
+            public string ByName { get; set; }
+            [QueryParameter(Name = "by-attribute")]
+            public string ByAttribute { get; set; }
+            public string NotToBeSerialized { get; set; }
+        }
+    }
+}
