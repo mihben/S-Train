469f7f3e68e54bea5f998dd1273a1088f5b038cd
Mihucza Bence <mihben@gmail.com>
2022-09-18 19:27:48 +0200

    Implemented AttributiveHeaderParameterBinder

---
+++ src/STrain.CQS.Http/RequestSending/Binders/Attributive/AttributiveHeaderParameterBinder.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using STrain.CQS.Attributes.RequestSending.Http.Parameters;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+namespace STrain.CQS.Http.RequestSending.Binders.Attributive
+{
+    public class AttributiveHeaderParameterBinder : IHeaderParameterBinder
+    {
+        private readonly ILogger<AttributiveHeaderParameterBinder> _logger;
+
+        public AttributiveHeaderParameterBinder(ILogger<AttributiveHeaderParameterBinder> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task BindAsync<TRequest>(TRequest request, HttpRequestHeaders headers, CancellationToken cancellationToken)
+            where TRequest : IRequest
+        {
+            _logger.LogDebug("Binding header parameters");
+
+            var type = typeof(TRequest);
+            if (type.GetCustomAttribute<HeaderParameterAttribute>() is not null) request.SerializeToHeaders(type.GetProperties(BindingFlags.Public | BindingFlags.Instance), headers);
+            else request.SerializeToHeaders(type.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                                                        .Where(p => p.GetCustomAttribute<HeaderParameterAttribute>() is not null), headers);
+
+            return Task.CompletedTask;
+        }
+    }
+
+    internal static class AttributiveHeaderParameterBinderExtensions
+    {
+        public static void SerializeToHeaders<TRequest>(this TRequest request, IEnumerable<PropertyInfo> properties, HttpRequestHeaders headers)
+        {
+            foreach (var property in properties)
+            {
+                var attribute = property.GetCustomAttribute<HeaderParameterAttribute>();
+                var value = property.GetValue(request);
+                if (value is not null) headers.Add(attribute?.Name ?? property.Name, value.ToString());
+            }
+        }
+    }
+}
---
+++ src/STrain.CQS.Test.Unit/Http/RequestSending/AttributiveHeaderParameterBinderTest.cs
@@ -0,0 +1,106 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using STrain.CQS.Attributes.RequestSending.Http.Parameters;
+using STrain.CQS.Http.RequestSending.Binders.Attributive;
+using STrain.CQS.Test.Unit.CQS;
+using Xunit.Abstractions;
+
+namespace STrain.CQS.Test.Unit.Http.RequestSending
+{
+    public class AttributiveHeaderParameterBinderTest
+    {
+
+        private readonly ILogger<AttributiveHeaderParameterBinder> _logger;
+
+        public AttributiveHeaderParameterBinderTest(ITestOutputHelper outputHelper)
+        {
+            _logger = new LoggerFactory()
+                          .AddXUnit(outputHelper)
+                          .CreateLogger<AttributiveHeaderParameterBinder>();
+        }
+
+        private AttributiveHeaderParameterBinder CreateSUT()
+        {
+            return new AttributiveHeaderParameterBinder(_logger);
+        }
+
+        [Fact(DisplayName = "[UNIT][AHPB-001] - Bind request")]
+        public async Task AttributiveHeaderParameterBinder_BindAsync_BindObject()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var message = new HttpRequestMessage();
+            var request = new Fixture().Create<HeaderParameterRequest>();
+
+            // Act
+            await sut.BindAsync(request, message.Headers, default);
+
+            // Assert
+            Assert.Collection(message.Headers, h => Assert.True(h.Key.Equals(nameof(HeaderParameterRequest.ByName)) && h.Value.First().Equals(request.ByName)),
+                                                                       h => Assert.True(h.Key.Equals("by-attribute") && h.Value.First().Equals(request.ByAttribute)));
+        }
+
+        [Fact(DisplayName = "[UNIT][AHPB-002] - Bind propertes")]
+        public async Task AttributiveHeaderParameterBinder_BindAsync_BindProperties()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var message = new HttpRequestMessage();
+            var request = new Fixture().Create<HeaderPropertyParameterRequest>();
+
+            // Act
+            await sut.BindAsync(request, message.Headers, default);
+
+            // Assert
+            Assert.Collection(message.Headers, h => Assert.True(h.Key.Equals(nameof(HeaderPropertyParameterRequest.ByName)) && h.Value.First().Equals(request.ByName)),
+                                                                       h => Assert.True(h.Key.Equals("by-attribute") && h.Value.First().Equals(request.ByAttribute)));
+        }
+
+        [Fact(DisplayName = "[UNIT][AHPB-003] - Skip null property")]
+        public async Task AttributiveHeaderParameterBinder_BindAsync_SkipNullProperties()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var message = new HttpRequestMessage();
+            var request = new Fixture().Build<HeaderParameterRequest>().Without(r => r.ByName).Without(r => r.ByAttribute).Create();
+
+            // Act
+            await sut.BindAsync(request, message.Headers, default);
+
+            // Assert
+            Assert.Empty(message.Headers);
+        }
+
+        [Fact(DisplayName = "[UNIT][AHPB-003] - Without header parameter")]
+        public async Task AttributiveHeaderParameterBinder_BindAsync_WithoutHeaderParameter()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var message = new HttpRequestMessage();
+            var request = new Fixture().Create<TestRequest>();
+
+            // Act
+            await sut.BindAsync(request, message.Headers, default);
+
+            // Assert
+            Assert.Empty(message.Headers);
+        }
+
+        [HeaderParameter]
+        private record HeaderParameterRequest : IRequest
+        {
+            public string ByName { get; set; }
+            [HeaderParameter(Name = "by-attribute")]
+            public string ByAttribute { get; set; }
+        }
+
+        private record HeaderPropertyParameterRequest : IRequest
+        {
+            [HeaderParameter]
+            public string ByName { get; set; }
+            [HeaderParameter(Name = "by-attribute")]
+            public string ByAttribute { get; set; }
+            public string NotToBeSerialized { get; set; }
+        }
+    }
+}
