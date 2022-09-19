b4ca7778e1e7be3851c0b1c664c29bf309f607f3
Mihucza Bence <mihben@gmail.com>
2022-09-19 12:52:09 +0200

    Implemented AttributiveBodyParameterBinder

---
+++ src/STrain.CQS.Http/RequestSending/Binders/Attributive/AttributiveBodyParameterBinder.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Net.Mime;
+using System.Reflection;
+using System.Text;
+
+namespace STrain.CQS.Http.RequestSending.Binders.Attributive
+{
+    public class AttributiveBodyParameterBinder : IBodyParameterBinder
+    {
+        private readonly ILogger<AttributiveBodyParameterBinder> _logger;
+        private readonly JsonSerializerSettings _serializerSettings = new()
+        {
+            ContractResolver = new AttributiveBodyParameterContractResolver()
+        };
+
+        public AttributiveBodyParameterBinder(ILogger<AttributiveBodyParameterBinder> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<HttpContent?> BindAsync<TRequest>(TRequest request, CancellationToken cancellationToken)
+            where TRequest : IRequest
+        {
+            if (request is null) throw new ArgumentNullException(nameof(request));
+
+            _logger.LogDebug("Binding body parameter");
+
+            var body = JsonConvert.SerializeObject(request, _serializerSettings);
+            if (body.Equals("{}")) return Task.FromResult<HttpContent?>(null);
+
+            return Task.FromResult((HttpContent?)new StringContent(body, Encoding.UTF8, MediaTypeNames.Application.Json));
+        }
+    }
+
+    internal class AttributiveBodyParameterContractResolver : DefaultContractResolver
+    {
+        protected override List<MemberInfo> GetSerializableMembers(Type objectType)
+        {
+            if (objectType.GetCustomAttribute<BodyParameterAttribute>() is not null) return base.GetSerializableMembers(objectType);
+
+            return objectType.GetMembers(BindingFlags.Instance | BindingFlags.Public)
+                                .Where(m => m.GetCustomAttribute<BodyParameterAttribute>() is not null).ToList();
+        }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var result = base.CreateProperty(member, memberSerialization);
+            var attribute = member.GetCustomAttribute<BodyParameterAttribute>();
+            if (attribute?.Name is not null) result.PropertyName = attribute.Name;
+
+            return result;
+        }
+    }
+}
---
+++ src/STrain.CQS.Test.Unit/Http/RequestSending/AttributiveBodyParameterBinderTest.cs
@@ -0,0 +1,117 @@
+﻿using AutoFixture;
+using Microsoft.Extensions.Logging;
+using STrain.CQS.Http.RequestSending.Binders.Attributive;
+using STrain.CQS.Test.Unit.CQS;
+using System.Net.Mime;
+using System.Text;
+using Xunit.Abstractions;
+
+namespace STrain.CQS.Test.Unit.Http.RequestSending
+{
+    public class AttributiveBodyParameterBinderTest
+    {
+
+        private readonly ILogger<AttributiveBodyParameterBinder> _logger;
+
+        public AttributiveBodyParameterBinderTest(ITestOutputHelper outputHelper)
+        {
+            _logger = new LoggerFactory()
+                          .AddXUnit(outputHelper)
+                          .CreateLogger<AttributiveBodyParameterBinder>();
+        }
+
+        private AttributiveBodyParameterBinder CreateSUT()
+        {
+            return new AttributiveBodyParameterBinder(_logger);
+        }
+
+        [Fact(DisplayName = "[UNIT][ABPB-001] - Bind request")]
+        public async Task AttributiveBodyParameterBinder_BindAsync_BindRequest()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var request = new Fixture().Create<BodyParameterRequest>();
+
+            // Act
+            var result = await sut.BindAsync(request, default);
+
+            // Assert
+            var content = Assert.IsType<StringContent>(result);
+            Assert.Equal(request.AsJson(), await content.ReadAsStringAsync());
+            Assert.Equal(Encoding.UTF8.WebName, content.Headers.ContentType.CharSet);
+            Assert.Equal(MediaTypeNames.Application.Json, content.Headers.ContentType.MediaType);
+        }
+
+        [Fact(DisplayName = "[UNIT][ABPB-002] - Bind property")]
+        public async Task AttributiveBodyParameterBinder_BindAsync_BindProperty()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var request = new Fixture().Create<PropertyParameterRequest>();
+
+            // Act
+            var result = await sut.BindAsync(request, default);
+
+            // Assert
+            var content = Assert.IsType<StringContent>(result);
+            Assert.Equal(request.AsJson(), await content.ReadAsStringAsync());
+            Assert.Equal(Encoding.UTF8.WebName, content.Headers.ContentType.CharSet);
+            Assert.Equal(MediaTypeNames.Application.Json, content.Headers.ContentType.MediaType);
+        }
+
+        [Fact(DisplayName = "[UNIT][ABPB-003] - Skip body parameter")]
+        public async Task AttributiveBodyParameterBinder_BindAsync_SkipBodyParameter()
+        {
+            // Arrange
+            var sut = CreateSUT();
+            var request = new Fixture().Create<TestRequest>();
+
+            // Act
+            var result = await sut.BindAsync(request, default);
+
+            // Assert
+            Assert.Null(result);
+        }
+
+        [Fact(DisplayName = "[UNIT][ABPB-004] - Request is null")]
+        public async Task AttributiveBodyParameterBinder_BindAsync_RequestIsNull()
+        {
+            // Arrange
+            var sut = CreateSUT();
+
+            // Act
+            // Assert
+            await Assert.ThrowsAsync<ArgumentNullException>(async () => await sut.BindAsync<IRequest>(null!, default));
+        }
+
+        [BodyParameter]
+        internal record BodyParameterRequest : IRequest
+        {
+            public string ByName { get; set; }
+            [BodyParameter(Name = "by-attribute")]
+            public string ByAttribute { get; set; }
+        }
+
+        internal record PropertyParameterRequest : IRequest
+        {
+            [BodyParameter]
+            public string ByName { get; set; }
+            [BodyParameter(Name = "by-attribute")]
+            public string ByAttribute { get; set; }
+            public string NotToBeSerialized { get; set; }
+        }
+    }
+
+    internal static class AttributiveBodyParameterBinderTestExtensions
+    {
+        public static string AsJson(this AttributiveBodyParameterBinderTest.BodyParameterRequest request)
+        {
+            return $"{{\"{nameof(request.ByName)}\":\"{request.ByName}\",\"by-attribute\":\"{request.ByAttribute}\"}}";
+        }
+
+        public static string AsJson(this AttributiveBodyParameterBinderTest.PropertyParameterRequest request)
+        {
+            return $"{{\"{nameof(request.ByName)}\":\"{request.ByName}\",\"by-attribute\":\"{request.ByAttribute}\"}}";
+        }
+    }
+}
