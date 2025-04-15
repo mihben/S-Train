62e183d4ceb30b3c9daf2caa25e80719404b107d
Mihucza, Bence <mihben@gmail.com>
2025-04-15 19:35:25 +0200

    fix: Registered the right initializer

--- src/STrain.Eventing.RabbitMQ.NetCore/Builders/PublisherBuilder.cs
+++ src/STrain.Eventing.RabbitMQ.NetCore/Builders/PublisherBuilder.cs
@@ -1,39 +1,40 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using STrain.Eventing.NetCore.Initializers;
+using STrain.Eventing.Publishers;
 using STrain.Eventing.RabbitMQ.Wrappers;
 
 namespace STrain.Eventing.RabbitMQ.NetCore.Builders
 {
-    public class PublisherBuilder
-    {
-        public WebApplicationBuilder Builder { get; }
-        public string Key { get; }
+	public class PublisherBuilder
+	{
+		public WebApplicationBuilder Builder { get; }
+		public string Key { get; }
 
-        public PublisherBuilder(WebApplicationBuilder builder, string key)
-        {
-            Builder = builder;
-            Key = key;
-        }
+		public PublisherBuilder(WebApplicationBuilder builder, string key)
+		{
+			Builder = builder;
+			Key = key;
+		}
 
-        public PublisherBuilder AddWrapper<TWrapper>()
-            where TWrapper : class, IWrapper
-        {
-            Builder.Services.AddKeyedTransient<IWrapper, TWrapper>(Key);
+		public PublisherBuilder AddWrapper<TWrapper>()
+			where TWrapper : class, IWrapper
+		{
+			Builder.Services.AddKeyedTransient<IWrapper, TWrapper>(Key);
 
-            return this;
-        }
+			return this;
+		}
 
-        public PublisherBuilder AddGenericWrapper()
-        {
-            return AddWrapper<GenericWrapper>();
-        }
+		public PublisherBuilder AddGenericWrapper()
+		{
+			return AddWrapper<GenericWrapper>();
+		}
 
-        public PublisherBuilder Initialize()
-        {
-            Builder.Services.AddHostedService<PublisherInitializer>();
+		public PublisherBuilder Initialize()
+		{
+			Builder.Services.AddHostedService(provider => new PublisherInitializer(provider.GetRequiredKeyedService<IPublisher>(Key));
 
-            return this;
-        }
-    }
+			return this;
+		}
+	}
 }
